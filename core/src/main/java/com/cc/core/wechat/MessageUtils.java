b8094cdcb39017c8a52ab5ecf16ba92bb539c3cd
Turning Thomas <ichengc@tutanota.com>
2019-02-20 22:08:49 +0800

    refine

---
+++ core/src/main/java/com/cc/core/wechat/MessageUtils.java
@@ -0,0 +1,13 @@
+package com.cc.core.wechat;
+
+public class MessageUtils {
+    private MessageUtils(){}
+
+    public static String downloadImage(String message) {
+        return "";
+    }
+
+    public static String downloadVideo(String message) {
+        return "";
+    }
+}
