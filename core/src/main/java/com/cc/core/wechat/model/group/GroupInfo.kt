1542897fc88dd059c387cf82421c301d13d2c024
Turning Thomas <ichengc@tutanota.com>
2019-02-24 13:02:19 +0800

    add group model

--- core/src/main/java/com/cc/core/wechat/model/group/GroupInfo.kt
+++ core/src/main/java/com/cc/core/wechat/model/group/GroupInfo.kt
@@ -1,9 +1,13 @@
 package com.cc.core.wechat.model.group
 
+import com.google.gson.annotations.SerializedName
+
 class GroupInfo {
-    private var members: Map<String, String>? = null
+    @SerializedName("ServerId")
+    private var ServerId: String? = null
     private var groupName : String? = null
     private var groupWechatId :String? = null
     private var groupNotice:String?= null
-    private var memberList:String?=null
+    private var roomownerId: String? = null
+    private var memberList:List<GroupUser>?=null
 }
