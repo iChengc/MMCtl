653ee09e8e55e1ae44144f5d5f69953a2d3aef22
Turning Thomas <ichengc@tutanota.com>
2019-03-01 22:23:25 +0800

    add avoid update

---
+++ core/src/main/java/com/cc/core/wechat/hook/AvoidUpdateHooks.java
@@ -0,0 +1,12 @@
+package com.cc.core.wechat.hook;
+
+import com.cc.core.xposed.BaseXposedHook;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AvoidUpdateHooks extends BaseXposedHook {
+    @Override
+    public void hook(@NotNull ClassLoader classLoader) {
+
+    }
+}
